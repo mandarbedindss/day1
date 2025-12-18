d80eb1778592966742de9f1758bd095af23c6c6b
mandarbedindss <mandar.bedi@ndsofttech.com>
2025-12-18 09:44:20 +0530

    Day2 commit, continue remaining tasks..

--- my-react-starter/src/App.jsx
+++ my-react-starter/src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Login from './Components/Login'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  )
+function App(){
+  return(<><Login/></>)
 }
 
 export default App
